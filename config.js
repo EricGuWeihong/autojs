043d7af1b096f17d7a115b6be4703c6b12d62bc8
Eric Gu <weihong_gool@hotmail.com>
2018-10-13 17:45:48 +0800

    Initial version from S9+

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    "password": "3#Neptune",
+    "takeImg": "take.png",
+    "pattern_size": "3",
+    "max_retry_times": "5",
+    "timeout": "12000",
+    "min_time": "7:14:00",
+    "max_time": "7:15:50",
+    "max_swipe_times": "100",
+    "check_within_time": "5",
+    "help_img": "help.png"
+};
+
+module.exports = config;
